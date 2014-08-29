eec6507c52c273a6d634b290a9e55be5ab4a2a1a
Ivan Kovtunov <qovtunov@gmail.com>
2014-08-29 18:18:53 +0300

    added BoxWeigh; modified Main

---
+++ BoxWeight.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by kovtunov on 29/08/14.
+ */
+public class BoxWeight extends Box {
+
+    double weight;
+
+    BoxWeight(double w, double h, double d, double m) {
+
+        width = w;
+        height = h;
+        depth = d;
+        weight = m;
+    }
+}
--- Main.java
+++ Main.java
@@ -32,6 +32,15 @@ public class Main {
         vol = myclone.volume();
         System.out.println("myclone = " + vol);*/
 
+                        // BoxWeight (EXTENDS)
+        /*BoxWeight mybox1 = new BoxWeight(10, 20, 15, 34.3 );
+        double vol;
+
+        vol = mybox1.volume();
+
+        System.out.println(vol);
+        System.out.println(mybox1.weight);*/
+
                         // ObLikeParam
         /*ObLikeParam ob1 = new ObLikeParam(100, 22);
         ObLikeParam ob2 = new ObLikeParam(100, 22);
@@ -41,10 +50,10 @@ public class Main {
         System.out.println("ob1 == ob3: " + ob1.equals(ob3));*/
 
                         // Factorial
-        Factorial f = new Factorial();
+        /*Factorial f = new Factorial();
 
         System.out.println("f3: " + f.fact(3));
         System.out.println("f4: " + f.fact(4));
-        System.out.println("f5: " + f.fact(5));
+        System.out.println("f5: " + f.fact(5));*/
     }
 }
